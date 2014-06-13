483f3d53846edbbdcb1ba923f5bab255ea202d22
lizzb <huzzah.its.lizz@gmail.com>
2014-06-13 00:48:31 -0500

    Revert fd69ee4..5535d04
    This rolls back to commit fd69ee4a74e0475cab161db943ea4f298dbee3c8.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,69 +1,7 @@
 angular.module('idayIonic.controllers', [])
 
-//.controller('AppCtrl', function($scope, $rootScope, RecruitingCompanies) {
-// .controller('AppCtrl', function($scope, $state, $ionicModal) {
-
-.controller('AppCtrl', function($scope, $rootScope, $state, $ionicModal, RecruitingCompanies) {
-
-
-$ionicModal.fromTemplateUrl('templates/login.html', function(modal) {
-      $scope.loginModal = modal;
-    },
-    {
-      scope: $scope,
-      animation: 'slide-in-up',
-      focusFirstInput: true
-    }
-  );
-  //Be sure to cleanup the modal by removing it from the DOM
-  $scope.$on('$destroy', function() {
-    $scope.loginModal.remove();
-  });
-})
+.controller('AppCtrl', function($scope, $rootScope, RecruitingCompanies) {
   
-.controller('LoginCtrl', function($scope, $http, $state, AuthenticationService) {
-  $scope.message = "";
-  
-  $scope.user = {
-    username: null,
-    password: null
-  };
- 
-  $scope.login = function() {
-    AuthenticationService.login($scope.user);
-  };
- 
-  $scope.$on('event:auth-loginRequired', function(e, rejection) {
-    $scope.loginModal.show();
-  });
- 
-  $scope.$on('event:auth-loginConfirmed', function() {
-   $scope.username = null;
-   $scope.password = null;
-     $scope.loginModal.hide();
-  });
-  
-  $scope.$on('event:auth-login-failed', function(e, status) {
-    var error = "Login failed.";
-    if (status == 401) {
-      error = "Invalid Username or Password.";
-    }
-    $scope.message = error;
-  });
- 
-  $scope.$on('event:auth-logout-complete', function() {
-    $state.go('app.home', {}, {reload: true, inherit: false});
-  }); 
-
-
-
-
-
-
-
-
-
-
 
   $rootScope.all_companies = RecruitingCompanies.all();
   //$rootScope.company_map = RecruitingCompanies.assignBooths();
@@ -221,27 +159,6 @@ $rootScope.userFilter = function (company) {
 })
 
 
-.controller('HomeCtrl', function($ionicViewService) {
-  // This a temporary solution to solve an issue where the back button is displayed when it should not be.
-  // This is fixed in the nightly ionic build so the next release should fix the issue
-  $ionicViewService.clearHistory();
-})
-
-.controller('CustomerCtrl', function($scope, $state, $http) {
-    $scope.customers = [];
-    
-    $http.get('https://customers')
-        .success(function (data, status, headers, config) {
-            $scope.customers = data;
-        })
-        .error(function (data, status, headers, config) {
-            console.log("Error occurred.  Status:" + status);
-        });
-})
- 
-.controller('LogoutCtrl', function($scope, AuthenticationService) {
-    AuthenticationService.logout();
-})
 
 
 
